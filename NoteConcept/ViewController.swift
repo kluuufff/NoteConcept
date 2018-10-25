635bac94b84b6b90937ff0a72f960abc165de8e5
iHope <panterasls123@gmail.com>
2018-10-25 13:55:04 +0300

    Initial Commit

---
+++ NoteConcept/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoteConcept
+//
+//  Created by Nadiya on 10/25/18.
+//  Copyright © 2018 Nadiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
